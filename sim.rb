b9b815680d71991c0c0d480a9a23673725c79c35
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-10-15 21:16:29 -0400

    Second Commitgit add .git add .!

--- sim.rb
+++ sim.rb
@@ -8,21 +8,25 @@ class Simulator do
 
 	def run(n)
 		@run_length = n
+		ticks = []
 		n.times do |i|
 			@run_percent = i/@run_length
-			tick i
+			ticks << tick i
 			sleep @delay
 		end
 	end
 
 	def tick(n)
-		@accel = Math.sin(n)
+		data = {}
+		data[:a] = ... # Accel
+		data[:v] = ... # Velocity
+		data[:x] = ... # Postion x in tube
+		data[:y] = ... # Vertical Postion
+		data[:z] = ... # Horizontal Postion
+
+		return data
 	end
 
 end
 
-while true do
-
-
-	sleep 1
-end
+Simulator.new.run(500)

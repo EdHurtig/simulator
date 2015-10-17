e1ea7fb0c3c01262876ce8f3733ca0eca4a4da5d
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-10-16 22:07:24 -0400

    More changes to make it work

--- sim.rb
+++ sim.rb
@@ -1,9 +1,15 @@
 require 'unirest'
-class Simulator 
-	@delay = 1
-
+class Simulator
 	def initialize(speed = 1)
 		@delay = 1 / speed
+		@template = {
+			a: 0.0,
+			v: 0.0,
+			x: 0.0,
+			y: 0.0,
+			z: 0.0
+		}
+		@data = []
 	end
 
 	def run(n)
@@ -17,7 +23,9 @@ class Simulator
 	end
 
 	def tick(n)
-		data = {}
+		data = @template
+
+		# Come up with complete BS for datapoints
 		data[:a] = (2 + Math.cos(n / Math::PI) / (2 * Math::PI ** 2) + \
 					(2 * Math.cos((2 * n) / Math::PI) / Math::PI ** 2) / \
 				    (2 * Math.sqrt(2 * n + Math.sin(n / Math::PI) / (2 * Math::PI) + \
@@ -33,11 +41,33 @@ class Simulator
 
 	def push(data)
 		boday = data.inject("") do |acc, (k, v)|
-			acc += "#{k}, pod=0001 value=#{v}\n"
+			acc += "#{k},pod=0001 value=#{v} #{timestamp}\n"
 		end
 
-		Unirest.post("http://localhost:8086/write?db=mydb", parameters: boday)
 		puts boday
+		resp = Unirest.post("http://stage-influxdb-1c:8086/write?db=metrics", parameters: boday)
+		puts "#{resp.code} #{resp.body}"
+		if resp.code == 204
+			save data
+		end
+	end
+
+	def timestamp
+		require 'time'
+		DateTime.now.strftime('%s%9N')
+		#DateTime.now.rfc3339(9)
+	end
+
+	def save(data)
+		@data << data
+	end
+
+	def get(index)
+		if @data[index]
+			return @data[index]
+		else
+			return @template
+		end
 	end
 end
 
